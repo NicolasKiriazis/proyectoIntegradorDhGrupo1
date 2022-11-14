e6c307616cf9384d2d4dfa2c05485e44837a0d7f
irovagna <irovagna@gmail.com>
2022-11-13 21:14:58 -0300

    Lista de usuarios e incorporacion de Perfil de usuraio

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,11 +7,39 @@ const usersFilePath = path.join(__dirname, '../data/userDataBase.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 let controller = {
+
     index: (req,res) =>{
         res.render('users/users', {
             users
         })
-    }
+    },
+
+    register: (req,res)=>{
+        res.render('users/register');
+    },
+
+    registered: function (req, res) { 
+    },
+
+    login (req,res) {
+        res.render('users/login');
+    },
+
+    profile: (req, res) => {
+
+        let user = users.find(user => user.id ==  req.params.id)
+
+        res.render('users/userProfile', {
+            user
+        })
+    },
+
+    destroy : (req, res) => {
+		let id = req.params.id;
+		let finalUsers = users.filter(user => user.id != id);
+		fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, ' '));
+		res.redirect('/users');
+	}
 }
 
 module.exports = controller;
--- src/router/usersRouter.js
+++ src/router/usersRouter.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const usersController = require('../controllers/usersController')
+const usersController = require('../controllers/usersController');
 
 
-router.get("/", usersController.index)
+router.get("/", usersController.index);
 
+router.get("/profile/:id", usersController.profile);
+
+router.delete('/delete/:id', usersController.destroy);
 
 module.exports = router;
