54320298a4567758e81f8baf44d5443697bb5958
Ignacio Rovagna <irovagna@gmail.com>
2023-01-16 18:48:04 -0300

    Correcciones finales

--- src/middlewares/productValidation.js
+++ src/middlewares/productValidation.js
@@ -2,6 +2,7 @@ const {body}=require("express-validator");
 
 const productValidation = [
     body('name').notEmpty().withMessage(" Debes ingresar un nombre del producto").bail(),
+    // AGREGAR CANTIDAD
     body('price').notEmpty().withMessage(" Debes ingresar una valor").bail(),
     body('discount').optional({checkFalsy: true}).isInt({min:0,max:100}).withMessage(" Debes ingresar una valor entre 0 y 100").bail(),
     
