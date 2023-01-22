31588988d10e848ccbe868606e00b5448d1e1f46
Nicolas Kiriazis <nicolaskiriazis@gmail.com>
2023-01-22 20:22:45 -0300

    Creando aplicacion react

---
+++ my-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ContentWrapper from './components/ContentWrapper';
+
+function App() {
+  return (
+    <>
+    <ContentWrapper/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-dashboard/src/components/dataBaseComponents/ContentDbComponets.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LastProductCreate from './LastProductCreate'
+import ProductsComponets from './ProductsComponets'
+
+const ContentDbComponets = () => {
+    return (
+    <>
+    
+    <ProductsComponets/>
+    // <LastProductCreate/>
+    
+    </>
+    )
+}
+
+export default ContentDbComponets
